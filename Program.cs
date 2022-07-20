04f64beeb6aa00e457d61eaa75c5fd3390a66f2b
Mariya <lovemerilove@inbox.com>
2022-07-21 01:53:59 +0500

    Решение третьей задачи №23

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
-
+/*
 double Lenth3D (double x1, double x2, double y1, double y2, double z1, double z2)
 {
    double Lenth3D = Math.Sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1)+(z2 - z1)*(z2 - z1));
@@ -24,4 +24,26 @@ Console.Write("Введите значение координаты z точки
 int z2 = Convert.ToInt32(Console.ReadLine());
 
 double Lenth = Lenth3D(x1, x2, y1, y2, z1, z2);
-Console.WriteLine($"Расстояние между точками = {Lenth}");
+Console.WriteLine($"Расстояние между точками = {Lenth}");*/
+
+
+
+/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+void NambersCube (int num)
+{
+   int current = 1;
+   while(current<=num)
+   {
+      Console.WriteLine($"{current} = {current*current*current}");
+      current++;
+   }
+}
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NambersCube(number);
